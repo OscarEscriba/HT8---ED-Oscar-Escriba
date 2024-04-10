9064dc268355ce80f2ee328b42f62ebd418de92a
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-04-09 23:54:57 -0600

    JavaDoc

--- HT8OscarEscriba/src/main/java/com/mycompany/ht8oscarescriba/VectorHeap.java
+++ HT8OscarEscriba/src/main/java/com/mycompany/ht8oscarescriba/VectorHeap.java
@@ -9,27 +9,47 @@ package com.mycompany.ht8oscarescriba;
  * @author Oscar Escriba
  */
 import java.util.Vector;
-
+/**
+ * Implementación de una cola de prioridad utilizando un montículo basado en un Vector.
+ * @param <E> el tipo de elementos que se almacenan en la cola de prioridad.
+ */
 public class VectorHeap<E extends Comparable<E>> implements PriorityQueues<E> {
-
+ /** Los datos, mantienen el orden del montículo */
     protected Vector<E> data; // Los datos, mantienen el orden del montículo
-
+    /**
+     * Constructor que inicializa un nuevo VectorHeap.
+     */
     public VectorHeap() {
         data = new Vector<>();
     }
-
+    /**
+     * Método que devuelve el índice del padre de un nodo dado.
+     * @param i el índice del nodo.
+     * @return el índice del padre del nodo.
+     */
     protected static int parent(int i) {
         return (i - 1) / 2;
     }
-
+      /**
+     * Método que devuelve el índice del hijo izquierdo de un nodo dado.
+     * @param i el índice del nodo.
+     * @return el índice del hijo izquierdo del nodo.
+     */
     protected static int left(int i) {
         return 2 * i + 1;
     }
-
+    /**
+     * Método que devuelve el índice del hijo derecho de un nodo dado.
+     * @param i el índice del nodo.
+     * @return el índice del hijo derecho del nodo.
+     */
     protected static int right(int i) {
         return (2 * i + 1) + 1;
     }
-
+    /**
+     * Método que ajusta el montículo hacia arriba a partir de una hoja dada.
+     * @param leaf el índice de la hoja.
+     */
     protected void percolateUp(int leaf) {
         int parent = parent(leaf);
         E value = data.get(leaf);
@@ -40,28 +60,42 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueues<E> {
         }
         data.set(leaf, value);
     }
-
+     /**
+     * Añade un elemento a la cola de prioridad.
+     * @param value el elemento a añadir.
+     */
     @Override
     public void add(E value) {
         data.add(value);
         percolateUp(data.size() - 1);
     }
-
+    /**
+     * Comprueba si la cola de prioridad está vacía.
+     * @return true si la cola de prioridad está vacía, false de lo contrario.
+     */
     @Override
     public boolean isEmpty() {
         return data.isEmpty();
     }
-
+    /**
+     * Devuelve el tamaño de la cola de prioridad.
+     * @return el tamaño de la cola de prioridad.
+     */
     @Override
     public int size() {
         return data.size();
     }
-
+    /**
+     * Elimina todos los elementos de la cola de prioridad.
+     */
     @Override
     public void clear() {
         data.clear();
     }
-
+    /**
+     * Ajusta el montículo hacia abajo a partir de la raíz dada.
+     * @param root el índice de la raíz.
+     */
     protected void pushDownRoot(int root) {
         int heapSize = data.size();
         E value = data.get(root);
@@ -85,12 +119,18 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueues<E> {
             }
         }
     }
-
+    /**
+     * Devuelve el primer elemento de la cola de prioridad sin eliminarlo.
+     * @return el primer elemento de la cola de prioridad.
+     */
     @Override
     public E getFirst() {
         return data.elementAt(0);
     }
-
+    /**
+     * Elimina y devuelve el primer elemento de la cola de prioridad.
+     * @return el primer elemento de la cola de prioridad.
+     */
     @Override
     public E remove() {
         E minVal = getFirst();
@@ -101,7 +141,10 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueues<E> {
         }
         return minVal;
     }
-
+    /**
+     * Devuelve una representación de cadena de la cola de prioridad.
+     * @return una cadena que representa la cola de prioridad.
+     */
     @Override
     public String toString() {
         return "VectorHeap{" + "data=" + data + '}';
--- HT8OscarEscriba/src/main/java/com/mycompany/ht8oscarescriba/Vheap.java
+++ HT8OscarEscriba/src/main/java/com/mycompany/ht8oscarescriba/Vheap.java
@@ -9,41 +9,64 @@ package com.mycompany.ht8oscarescriba;
  * @author Oscar Escriba
  */
 import java.util.PriorityQueue;
-
+/**
+ * Implementación de una cola de prioridad utilizando PriorityQueue de Java Collection Framework.
+ * @param <E> el tipo de elementos que se almacenan en la cola de prioridad.
+ */
 public class Vheap<E extends Comparable<E>> extends PriorityQueue<E> implements NewInterface<E> {
-
+ /**
+     * Constructor que inicializa un nuevo Vheap.
+     */
   public Vheap(){
         super();
     }
-
+  /**
+     * Devuelve el primer elemento de la cola de prioridad sin eliminarlo.
+     * @return el primer elemento de la cola de prioridad.
+     */
     @Override
     public E getFirst() {
         return super.peek();
     }
-
+    /**
+     * Elimina y devuelve el primer elemento de la cola de prioridad.
+     * @return el primer elemento de la cola de prioridad.
+     */
     public E remove(){
         return super.remove();
     }
     // pre: !isEmpty()
     // post: returns and removes minimum value from queue
-
+    /**
+     * Añade un elemento a la cola de prioridad.
+     * @param value el elemento a añadir.
+     * @return true si el elemento se añade correctamente, false si no se pudo añadir.
+     */
     @Override
     public boolean add(E value){
         return super.add(value);
     }
     // pre: value is non-null comparable
     // post: value is added to priority queue
-
+     /**
+     * Comprueba si la cola de prioridad está vacía.
+     * @return true si la cola de prioridad está vacía, false de lo contrario.
+     */
     public boolean isEmpty(){
         return super.isEmpty();
     }
     // post: returns true iff no elements are in queue
-
+    /**
+     * Devuelve el número de elementos en la cola de prioridad.
+     * @return el número de elementos en la cola de prioridad.
+     */
     public int size(){
         return super.size();
     }
     // post: returns number of elements within queue
-
+     /**
+     * Elimina todos los elementos de la cola de prioridad.
+     */
     public void clear(){
         super.clear();
     }
